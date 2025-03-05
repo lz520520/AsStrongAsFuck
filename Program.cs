f89a2463bb34ef1927f8beb1f905c74fa64bdafe
lz520520 <610188221@qq.com>
2025-03-05 19:11:05 +0800

    添加项目文件。

---
+++ Program.cs
@@ -0,0 +1,75 @@
+﻿using CommandLine;
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsStrongAsFuck
+{
+    class Program
+    {
+        public static Worker Worker { get; set; }
+        public class Options
+        {
+            
+            [Option('o', "obfuscators", Default = null, Required = false, HelpText = "List of obfuscation methods to use (numeric string 1-9)")]
+            public string Obfuscators { get; set; }
+
+            [Option('f', "file", Required = false,Default ="", HelpText = "Obfuscate a file on disk")]
+            public string InFile { get; set; } 
+
+            [Option('h', "help", Required = false, HelpText = "Print this help")]
+            public bool Help { get; set; } 
+        }
+
+        public static void Main(string[] args)
+        {
+            var watch = Stopwatch.StartNew();
+
+            Parser.Default.ParseArguments<Options>(args)
+                .WithParsed(RunOptions)
+                .WithNotParsed(HandleParseError);
+
+            watch.Stop();
+
+        }
+
+        static void PrintHelp() {
+            var woker = new Worker();
+            Console.WriteLine("Usage: asaf.exe -f <path> -o <obfuscators>");
+            Console.WriteLine("  Ex: asaf.exe -f .\\test.exe -o 134");
+            Console.WriteLine("Available obfuscators: ");
+            for (int i = 0; i < woker.Obfuscations.Count; i++)
+            {
+                Console.WriteLine(i + 1 + ") " + woker.Obfuscations[i]);
+            }
+
+        }
+
+        static void RunOptions(Options opts)
+        {
+
+            if (opts.Help || opts.InFile.Length == 0) {
+                PrintHelp();
+                Environment.Exit(0);
+            }
+
+            var Worker = new Worker(opts.InFile);
+
+            Worker.ExecuteObfuscations(opts.Obfuscators);
+            Worker.Save();
+        }
+
+        static void HandleParseError(IEnumerable<Error> errs)
+        {
+            foreach (Error err in errs)
+            {
+                Console.Error.WriteLine(err.ToString());
+            }
+        }
+    }
+}
